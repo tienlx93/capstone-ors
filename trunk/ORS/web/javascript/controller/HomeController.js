cc64d830bb1431c1290467349c3e896cb92d7f73
quoc <quoc.nguyen@ludiino.com>
2015-06-29 10:06:20 +0700

    <quocnvh> truncate text for office description

--- trunk/ORS/web/javascript/controller/HomeController.js
+++ trunk/ORS/web/javascript/controller/HomeController.js
@@ -6,7 +6,11 @@ controllers.controller('HomeController', ['$scope','$location', 'Api',
         $scope.getNewOffice = function () {
             Api.getNewOffice(function(data) {
                 if (data) {
-                    $scope.listNewOffice = data
+                    $scope.listNewOffice = data;
+                    for(var i = 0; i < $scope.listNewOffice.length; i++) {
+                        $scope.listNewOffice[i].description = $scope.listNewOffice[i].description.length > 200 ?
+                        $scope.listNewOffice[i].description.substring(0, 200) + "..." : $scope.listNewOffice[i].description;
+                    }
                 }
             })
         };
